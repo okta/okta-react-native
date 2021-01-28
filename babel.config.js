e29eb5a97b9d7a48d565566db967437f0d645841
Oleg Gnidets <oleg.gnidets@okta.com>
2021-01-28 09:27:49 +0000

    test: Add new tests, add new ESLint rules test: Fix code formatting according to ESLint in babel.config.js
    Cover index.js with 100% code coverage. Refactoring of some unit tests. Add build stage before running tests. Fix code formatting by adding new ESLint rules.
    
    Resolves: OKTA-355717 (#42)
    
    OKTA-355717
    <<<Jenkins Check-In of Tested SHA: e8c634c2f236744e628d4097cfd8ce9f11d3f2ab for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-react-native

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ["@babel/plugin-transform-async-to-generator"]
+  plugins: ['@babel/plugin-transform-async-to-generator']
 };
