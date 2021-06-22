3637d5238431a409bc3696a8e4cc9d1d8b92f61b
Shuo Wu <wushuo2010@gmail.com>
2021-06-22 18:47:59 -0400

    update bacon yml

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -10,11 +10,6 @@ if ! yarn lint:report; then
   exit ${TEST_FAILURE}
 fi
 
-if ! yarn dtslint; then
-  echo "DTS lint failed! Exiting..."
-  exit 1
-fi
-
 echo ${TEST_SUITE_TYPE} > ${TEST_SUITE_TYPE_FILE}
 echo ${TEST_RESULT_FILE_DIR} > ${TEST_RESULT_FILE_DIR_FILE}
 exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_SUCCEED_IF_NO_RESULTS}
