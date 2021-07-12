8828a559fbb69763ea5f9509c74b9e0884da840c
Oleg Gnidets <oleg.gnidets@okta.com>
2021-07-12 19:24:29 +0300

    fix: signOut never throws an error iOS

--- ios/OktaSdkBridge/ReactNativeOktaSdkBridge.m
+++ ios/OktaSdkBridge/ReactNativeOktaSdkBridge.m
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, Okta, Inc. and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019-Present, Okta, Inc. and/or its affiliates. All rights reserved.
  * The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
  *
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
@@ -35,7 +35,9 @@ RCT_EXTERN_METHOD(
   promiseRejecter:(RCTPromiseRejectBlock *)promiseRejecter
 )
 
-RCT_EXTERN_METHOD(signOut)
+RCT_EXTERN_METHOD(signOut:
+                  promiseResolver:(RCTPromiseResolveBlock *)promiseResolver
+                  promiseRejecter:(RCTPromiseRejectBlock *)promiseRejecter)
 
 RCT_EXTERN_METHOD(getAccessToken:(RCTPromiseResolveBlock *)promiseResolver promiseRejecter:(RCTPromiseRejectBlock *)promiseRejecter)
 
