68d6d3e3c80b189e09ac1aa4d0e2fceae5b94d15
Rajdeep Nanua <rajdeep.nanua@okta.com>
2023-10-27 17:00:15 -0400

    Fix iOS build

--- e2e/ios/E2EOktaReactNative/noop-file.swift
+++ e2e/ios/E2EOktaReactNative/noop-file.swift
@@ -1,4 +1,4 @@
-+//
-+// @generated
-+// A blank Swift file must be created for native modules with Swift files to work correctly.
-+//
+//
+// @generated
+// A blank Swift file must be created for native modules with Swift files to work correctly.
+//
